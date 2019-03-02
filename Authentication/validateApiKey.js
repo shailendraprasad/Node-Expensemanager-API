b47934e9e95457466a539daee51e3046985a4720
Shailendra Prasad <n.shailendraprasad@gmail.com>
2019-03-02 20:15:08 +0530

    API key validation

--- Authentication/validateApiKey.js
+++ Authentication/validateApiKey.js
@@ -3,9 +3,14 @@ import { APIKey } from '../config'
 //function to validate the apikey header value in the request
 var validateApiKey = (req, res, next) => {
     var apiKeyfromHeader = req.headers['apikey'];
+    //Dont validate the API key for the initial URL
+    if(req.url == "/"){
+        return res.status(200).json({ Message: "This is an API for Expense Manage App" });
+    }
     if (apiKeyfromHeader === APIKey) {
         next();
     }
+    
     else {
         return res.status(401).json({ Message: "API Key is not provided in the request" });
     }
